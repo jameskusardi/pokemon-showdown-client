12525b169af897efd0df2b2096422b823680ec4c
jameskusardi <risingSolaris@hotmail.com>
2021-08-18 17:45:24 -0700

    Add files via upload

---
+++ data/formats-data.ts
@@ -0,0 +1,186 @@
+export const FormatsData: {[k: string]: SpeciesFormatsData} = {
+	grizzeal: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	stampyro: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	hippothagoras: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	moleder: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	bombatross: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	galvia: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	calistaiji: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	setstorm: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	kheprise: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	weelpim: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	puddogre: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	doderror: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	strignight: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	coowoo: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	spectropa: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	crikeri: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	boxetta: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	kingphan: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	skuntomic: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	croantagion: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	gaggular: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	ultranaut: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	dragulonimbus: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	dragulare: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	dragulanche: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	scrittle: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	corruptrain: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	pasdovo: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	glasiosaur: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	bellophus: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	velocust: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	kuwengu: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	putango: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	rishelios: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	odonaga: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	solterra: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	lunterra: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	venomab: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	forganon: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	isladon: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	ooreina: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	ooreinaflare: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	rangdemos: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	baronglaiv: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	thundra: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+	eruptil: {
+		tier: "OU",
+		doublesTier: "(DOU)",
+	},
+};
